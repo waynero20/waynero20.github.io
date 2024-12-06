d6e54277345b2d0962bd8db54eec1791e9203e8e
Wayne Rondina <wayne@Waynes-MacBook-Air.local>
2024-12-06 18:05:07 +0800

    chore: Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
+  /* config options here
+  basePath: "",
+  output: "export", */
   reactStrictMode: true,
 };
 
